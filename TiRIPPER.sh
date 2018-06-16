7243d6ce13e79f27c60bc8b655eb27a33fe05d9b
Yongliang Yu <enigmaraul2@hotmail.com>
2018-06-16 01:01:26 -0400

    add playlist support

--- TiRIPPER.sh
+++ TiRIPPER.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #needs gsed, metaflac & ffmpeg (for flac encodes)
 
-
-
 #tidalSession="X-Tidal-SessionId: SESSIONKEYHERE"
-token=???
+#token=???
+play=0
 
 for var in "$@"
 do
@@ -14,11 +13,19 @@ echo "-->album ID?"
 read albumID
 fi
 
+if [ ${#albumID} -ge 10 ]; then   
+play=1
+fi
+
 #superfluous crap probably
 #-------------------------
 nullCheck=0
 while [ "$nullCheck" == 0 ];do
-html=$(curl -s --http1.1 -H  "$tidalSession" -H "X-Tidal-Token: $token" -H "User-Agent: TIDAL/362 CFNetwork/711.4.6 Darwin/14.0.0" "http://api.tidalhifi.com/v1/albums/$albumID/tracks?countryCode=US&limit=9999&orderDirection=ASC" | cat )
+if [ "$play" = 1 ]; then   
+html=$(curl -s --http1.1 -H  "$tidalSession" -H "X-Tidal-Token: $token" -H "User-Agent: TIDAL/362 CFNetwork/711.4.6 Darwin/14.0.0" "http://api.tidalhifi.com/v1/playlists/$albumID/items?countryCode=US&limit=100&orderDirection=ASC" | cat )
+else
+html=$(curl -s --http1.1 -H  "$tidalSession" -H "X-Tidal-Token: $token" -H "User-Agent: TIDAL/362 CFNetwork/711.4.6 Darwin/14.0.0" "http://api.tidalhifi.com/v1/albums/$albumID/tracks?countryCode=US&limit=100&orderDirection=ASC" | cat )
+fi
 maxItems=$(jq -r '.totalNumberOfItems' <<< "$html")
 if [ "$maxItems" == "null" ]; then
 echo "$albumID Null error."
@@ -30,14 +37,19 @@ fi
 done
 #-------------------------
 
-
 echo "----Selected Album has $maxItems tracks."
 
 if [ -n "$1" ];then
 index=1
 avoid=1
 avoidInput=1
+
+if [ "$play" = 1 ];then
+delm4a=0
+else
 delm4a=1
+fi
+
 else
 
 echo "-->index? (default 1)"
@@ -68,7 +80,11 @@ fi
 
 for (( i=0; i<maxItems; i++ ))
 do
+if [ "$play" = 1 ];then
+fastCheck=$(jq -r ".items[$i].item.audioQuality" <<< "$html")
+else
 fastCheck=$(jq -r ".items[$i].audioQuality" <<< "$html")
+fi
 if [ "$fastCheck" != "LOSSLESS" ] && [ "$fastCheck" != "HI_RES" ]; then
 echo "Track $((i+1)) not LOSSLESS! Type anything once to stop ripping and continue output."
 echo "$fastCheck"
@@ -85,22 +101,43 @@ if [ "$stop" = 1 ]; then
 exit
 fi
 
-
+if [ "$play" = 1 ];then
+while read -r name
+do
+trackName+=("$name");
+done < <(jq -r '.items[].item.title' <<< "$html")
+else
 while read -r name
 do
 trackName+=("$name");
 done < <(jq -r '.items[].title' <<< "$html")
+fi
 
-
+if [ "$play" = 1 ];then
+while read -r name
+do
+trackNumber+=("$name");
+done < <(jq -r '.items[].item.trackNumber' <<< "$html")
+else
 while read -r name
 do
 trackNumber+=("$name");
 done < <(jq -r '.items[].trackNumber' <<< "$html")
+fi
 
 ###split singles in different folder
-albumName=$(gsed -e 's@/@,@g' <<< $(jq -r '.items[0].album.title' <<< "$html")) 
+if [ "$play" = 1 ];then
+albumName="$albumID"
+else
+albumName=$(gsed -e 's@/@,@g' <<< $(jq -r '.items[0].album.title' <<< "$html"))
+fi
 
+
+#VA CHECK
 #--------
+if [ "$play" = 0 ];then
+
+
 if [ "$maxItems" -ge 3 ];then
 artistNameA=$(jq -r '.items[1].artist.name' <<< "$html")
 artistNameB=$(jq -r '.items[2].artist.name' <<< "$html")
@@ -113,11 +150,16 @@ fi
 else
 artistName=$(jq -r '.items[0].artist.name' <<< "$html")
 fi
+
+fi
 #--------
+
+if [ "$play" = 0 ];then
 yearF=$(jq -r ".items[0].streamStartDate" <<< "$html" | cut -c1-4)
+fi
 
 if [ "$delm4a" = 1 ];then
-mkdir "$artistName - $albumName ($yearF) [FLAC] web"
+mkdir "Playlist - $albumName [FLAC] web"
 else
 mkdir "$artistName - $albumName ($yearF) [ALAC] web"
 fi
@@ -129,18 +171,25 @@ chmod 777 RIPPERTEMP.txt
 rm RIPPERTEMP.txt
 fi
 
+
+if [ "$play" = 1 ];then
+jq '.items[].item.id' <<< "$html" >> RIPPERTEMP.txt
+else
 jq '.items[].id' <<< "$html" >> RIPPERTEMP.txt
+fi
 
 filename="RIPPERTEMP.txt"
 
 if ls cover.jpg 1> /dev/null 2>&1; then
 echo "Already Cover Exists."
 else
-coverLocation=$(jq -r '.items[0].album.cover' <<< "$html")
+
+if [ "$play" = 0 ];then
+coverLocation=$(jq -r '.item[0].album.cover' <<< "$html")
 imageURL="https://resources.tidal.com/images/${coverLocation//-//}/1280x1280.jpg"
 wget -O cover.jpg "$imageURL"
 fi
-
+fi
 
 while read -r line
 do
@@ -158,6 +207,8 @@ dindex=$((index-1))
 name="$line"
 newfix=$(gsed -e 's@/@,@g' <<< "${trackName[dindex]}")
 
+
+
 if ls "${findex} - $newfix.m4a" 1> /dev/null 2>&1;then
 echo "Already Exists."
 skip=1
@@ -166,16 +217,27 @@ echo "Already Exists."
 skip=1
 else
 wget --tries=2 -O "${findex} - $newfix.m4a" "$(curl -s -H  "$tidalSession" -H "X-Tidal-Token: $token" -H "User-Agent: TIDAL/362 CFNetwork/711.4.6 Darwin/14.0.0" "http://api.tidalhifi.com/v1/tracks/$name/streamurl?countryCode=US&soundQuality=LOSSLESS" | jq -r ."url")"
-fi
 
 if [ "$delm4a" = 1 ] && [ "$skip" = 0 ];then
+if [ "$play" = 1 ];then
+year=$(jq -r ".items[$dindex].item.streamStartDate" <<< "$html" | cut -c1-4)
+artistName=$(jq -r ".items[$dindex].item.artist.name" <<< "$html")
+albumNameN=$(jq -r ".items[$dindex].item.album.title" <<< "$html")
+ffmpeg -y -i "${findex} - ${trackName[dindex]}.m4a" -metadata title="${trackName[dindex]}" -metadata album="$albumNameN" -metadata track="${trackNumber[dindex]}" -metadata artist="$artistName" -metadata year="$year" -f flac "${findex} - ${trackName[dindex]}.flac" < /dev/null
+rm "${findex} - ${trackName[dindex]}.m4a"
+else
 year=$(jq -r ".items[$dindex].streamStartDate" <<< "$html" | cut -c1-4)
 artistName=$(jq -r ".items[$dindex].artist.name" <<< "$html")
 ffmpeg -y -i "${findex} - ${trackName[dindex]}.m4a" -metadata title="${trackName[dindex]}" -metadata album="$albumName" -metadata track="${trackNumber[dindex]}" -metadata artist="$artistName" -metadata year="$year" -f flac "${findex} - ${trackName[dindex]}.flac" < /dev/null
 rm "${findex} - ${trackName[dindex]}.m4a"
 fi
+
+
+fi
 avoid=$((avoid+1))
 index=$((index+1))
+
+fi
 
 else
 index=$((index+1))
@@ -183,8 +245,10 @@ fi
 
 done < "$filename"
 
+if [ "$delm4a" = 1 ];then
 echo 'Adding gain..'
-metaflac --add-replay-gain *.flac
+#metaflac --add-replay-gain *.flac
+fi
 
 chmod 777 RIPPERTEMP.txt
 rm RIPPERTEMP.txt
@@ -207,4 +271,3 @@ trackNumber=()
 cd ..
 done
 exit
-
