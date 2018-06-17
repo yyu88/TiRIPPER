b8d231cacdddd0715a3c667079e9e96db6c2bb58
Yongliang Yu <enigmaraul2@hotmail.com>
2018-06-17 02:46:29 -0400

    Delete login.sh

--- login.sh
+++
@@ -1,8 +0,0 @@
-#need your token too
-echo "-->email(before the @)?"
-read email
-echo "-->email server(after the @)?"
-read server
-echo "-->password?"
-read pw
-curl --silent -H "X-Tidal-Token: _DSTon1kC8pABnTw" -H "User-Agent: TIDAL/362 CFNetwork/711.4.6 Darwin/14.0.0" --data "username=$email%40$server&password=$pw&clientVersion=1.12.1&clientUniqueKey=" api.tidalhifi.com/v1/login/username | jq -r ".sessionId"
